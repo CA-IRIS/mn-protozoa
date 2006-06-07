26ab6de571da197471af31362978347aea6f45c3
Douglas Lau (doug.lau@dot.state.mn.us) <Douglas Lau (doug.lau@dot.state.mn.us)>
2006-06-07 16:40:39 -0500

    Interpret the pan/tilt values on input.

--- src/manchester.c
+++ src/manchester.c
@@ -3,6 +3,36 @@
 
 #define FLAG 0x80
 
+static inline int pt_bits(uint8_t *mess) {
+	return (mess[1] >> 4) & 0x03;
+}
+
+static inline int pt_speed(uint8_t *mess) {
+	return (mess[1] >> 1) & 0x07;
+}
+
+static int pan_value(uint8_t *mess) {
+	int cmnd = pt_bits(mess);
+	if(mess[2] & 0x02) {
+		if(cmnd == 0x02)
+			return -pt_speed(mess);
+		else if(cmnd == 0x03)
+			return pt_speed(mess);
+	}
+	return 0;
+}
+
+static int tilt_value(uint8_t *mess) {
+	int cmnd = pt_bits(mess);
+	if(mess[2] & 0x02) {
+		if(cmnd == 0x00)
+			return -pt_speed(mess);
+		else if(cmnd == 0x01)
+			return pt_speed(mess);
+	}
+	return 0;
+}
+
 static int manchester_read_message(struct buffer *rxbuf) {
 	uint8_t mess[3];
 	int i;
@@ -19,7 +49,8 @@ static int manchester_read_message(struct buffer *rxbuf) {
 	printf("%02x %02x %02x\n", mess[0], mess[1], mess[2]);
 	int receiver = (mess[0] & 0x03) << 6 | (mess[1] & 0x01) << 5 |
 		(mess[2] >> 2) & 0x1f;
-	printf("receiver %d\n", receiver + 1);
+	printf("receiver: %d pan: %d tilt: %d\n", receiver + 1,
+		pan_value(mess), tilt_value(mess));
 	return 0;
 }
 
