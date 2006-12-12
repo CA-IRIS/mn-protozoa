9b65868fb379612403cb029aff0e3828db3df58c
Douglas Lau (doug.lau@dot.state.mn.us) <Douglas Lau (doug.lau@dot.state.mn.us)>
2006-12-12 14:19:27 -0600

    Comment fix.

--- src/ccwriter.h
+++ src/ccwriter.h
@@ -6,7 +6,7 @@
 
 struct ccwriter {
 	unsigned int (*do_write) (struct ccwriter *w, struct ccpacket *p);
-	struct	log		*log;		/* logger */
+	struct	log		*log;		/* message logger */
 	struct	buffer		*txbuf;		/* transmit buffer */
 	int			base;		/* receiver address base */
 	int			range;		/* receiver address range */
