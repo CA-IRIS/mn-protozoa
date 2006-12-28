fa1263f04ecede60d0f0267e7274a8a1c8b631e9
Douglas Lau (doug.lau@dot.state.mn.us) <Douglas Lau (doug.lau@dot.state.mn.us)>
2006-12-28 14:48:23 -0600

    Added comments to buffer.c file.

--- src/buffer.c
+++ src/buffer.c
@@ -1,7 +1,7 @@
 #include "buffer.h"
-#include <string.h>
+#include <string.h>	/* for memmove */
 #include <unistd.h>	/* for read, write */
-#include <sys/errno.h>
+#include <sys/errno.h>	/* for errno */
 
 extern int errno;
 
@@ -21,6 +21,9 @@ struct buffer *buffer_init(struct buffer *buf, size_t n_bytes) {
 	return buf;
 }
 
+/*
+ * buffer_destroy	Destroy the previously initialized I/O buffer.
+ */
 void buffer_destroy(struct buffer *buf) {
 	free(buf->base);
 	buf->base = NULL;
@@ -29,27 +32,53 @@ void buffer_destroy(struct buffer *buf) {
 	buf->pout = NULL;
 }
 
+/*
+ * buffer_clear		Clear the contents of the I/O buffer.
+ */
 void buffer_clear(struct buffer *buf) {
 	buf->pin = buf->base;
 	buf->pout = buf->base;
 }
 
+/*
+ * buffer_available	Get the number of bytes available in the I/O buffer.
+ *
+ * return: number of bytes available
+ */
 inline size_t buffer_available(const struct buffer *buf) {
 	return buf->pin - buf->pout;
 }
 
+/*
+ * buffer_is_empty	Test if the I/O buffer is empty.
+ *
+ * return: true if buffer is empty; otherwise false
+ */
 inline bool buffer_is_empty(const struct buffer *buf) {
 	return buffer_available(buf) <= 0;
 }
 
+/*
+ * buffer_space		Get the space remaining in the I/O buffer.
+ *
+ * return: space (bytes) remaining in buffer
+ */
 inline size_t buffer_space(const struct buffer *buf) {
 	return buf->end - buf->pin;
 }
 
+/*
+ * buffer_is_full	Test if the I/O buffer is full.
+ *
+ * return: true if buffer is full; otherwise false
+ */
 inline bool buffer_is_full(const struct buffer *buf) {
 	return buffer_space(buf) <= 0;
 }
 
+/*
+ * buffer_compact	Compact the I/O buffer to free up more space.
+ */
 static inline void buffer_compact(struct buffer *buf) {
 	size_t a = buffer_available(buf);
 	memmove(buf->base, buf->pout, a);
@@ -57,6 +86,12 @@ static inline void buffer_compact(struct buffer *buf) {
 	buf->pin = buf->pout + a;
 }
 
+/*
+ * buffer_read		Read data from a file descriptor into the I/O buffer.
+ *
+ * fd: file descriptor to read data from
+ * return: number of bytes read; -1 on error (with errno set)
+ */
 ssize_t buffer_read(struct buffer *buf, int fd) {
 	size_t count = buffer_space(buf);
 	if(count <= 0) {
@@ -73,6 +108,12 @@ ssize_t buffer_read(struct buffer *buf, int fd) {
 	return n_bytes;
 }
 
+/*
+ * buffer_write		Write data from the I/O buffer to a file descriptor.
+ *
+ * fd: file descriptor to write data to
+ * return: number of bytes written; -1 on error (with errno set)
+ */
 ssize_t buffer_write(struct buffer *buf, int fd) {
 	size_t count = buffer_available(buf);
 	if(count <= 0) {
@@ -85,6 +126,12 @@ ssize_t buffer_write(struct buffer *buf, int fd) {
 	return n_bytes;
 }
 
+/*
+ * buffer_append	Append data to the I/O buffer.
+ *
+ * n_bytes: number of bytes to append
+ * return: pointer to the appended data, or NULL on error
+ */
 void *buffer_append(struct buffer *buf, size_t n_bytes) {
 	void *pin = buf->pin;
 	if(buffer_space(buf) < n_bytes) {
@@ -95,10 +142,20 @@ void *buffer_append(struct buffer *buf, size_t n_bytes) {
 	return pin;
 }
 
+/*
+ * buffer_current	Get the current position in the I/O buffer.
+ *
+ * return: pointer to the current buffer position
+ */
 inline void *buffer_current(struct buffer *buf) {
 	return buf->pout;
 }
 
+/*
+ * buffer_skip		Skip forward into the I/O buffer.
+ *
+ * n_bytes: number of bytes to skip forward
+ */
 void buffer_skip(struct buffer *buf, size_t n_bytes) {
 	buf->pout += n_bytes;
 	if(buf->pout >= buf->pin)
