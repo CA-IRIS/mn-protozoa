f8c51ff04c2fa8aa4bf702404b7549bdffdfba94
Douglas Lau (doug.lau@dot.state.mn.us) <Douglas Lau (doug.lau@dot.state.mn.us)>
2006-12-29 12:09:43 -0600

    Renamed struct config variables from c -> cfg.

--- src/config.c
+++ src/config.c
@@ -3,59 +3,62 @@
 #include "ccreader.h"
 #include "ccwriter.h"
 
-void config_init(struct config *c, const char *filename, struct log *log) {
-	c->filename = filename;
-	c->line = malloc(LINE_LENGTH);
-	c->chns = NULL;
-	c->n_channels = 0;
-	c->log = log;
-	c->out = NULL;
+void config_init(struct config *cfg, const char *filename, struct log *log) {
+	cfg->filename = filename;
+	cfg->line = malloc(LINE_LENGTH);
+	cfg->chns = NULL;
+	cfg->n_channels = 0;
+	cfg->log = log;
+	cfg->out = NULL;
 	if(log->stats) {
-		c->counter = malloc(sizeof(struct packet_counter));
-		counter_init(c->counter, log);
+		cfg->counter = malloc(sizeof(struct packet_counter));
+		counter_init(cfg->counter, log);
 	} else
-		c->counter = NULL;
+		cfg->counter = NULL;
 }
 
-static struct channel *config_find_channel(struct config *c, const char *name) {
+static struct channel *config_find_channel(struct config *cfg,
+	const char *name)
+{
 	int i;
-	for(i = 0; i < c->n_channels; i++) {
-		if(strcmp(name, c->chns[i].name) == 0)
-			return c->chns + i;
+	for(i = 0; i < cfg->n_channels; i++) {
+		if(strcmp(name, cfg->chns[i].name) == 0)
+			return cfg->chns + i;
 	}
 	return NULL;
 }
 
-static struct channel *config_new_channel(struct config *c, const char *name,
+static struct channel *config_new_channel(struct config *cfg, const char *name,
 	int extra)
 {
 	struct channel *chn;
-	c->n_channels++;
-	c->chns = realloc(c->chns, sizeof(struct channel) * c->n_channels);
-	if(c->chns == NULL)
+	cfg->n_channels++;
+	cfg->chns = realloc(cfg->chns,
+		sizeof(struct channel) * cfg->n_channels);
+	if(cfg->chns == NULL)
 		return NULL;
-	chn = c->chns + (c->n_channels - 1);
-	if(channel_init(chn, name, extra, c->log) == NULL) {
+	chn = cfg->chns + (cfg->n_channels - 1);
+	if(channel_init(chn, name, extra, cfg->log) == NULL) {
 		channel_log(chn, "Initialization error");
-		c->n_channels--;
+		cfg->n_channels--;
 		return NULL;
 	}
 	return chn;
 }
 
-static struct channel *config_get_channel(struct config *c, const char *name,
+static struct channel *config_get_channel(struct config *cfg, const char *name,
 	int extra)
 {
 	struct channel *chn;
 
-	chn = config_find_channel(c, name);
+	chn = config_find_channel(cfg, name);
 	if(chn)
 		return chn;
 	else
-		return config_new_channel(c, name, extra);
+		return config_new_channel(cfg, name, extra);
 }
 
-static int config_directive(struct config *c, const char *protocol_in,
+static int config_directive(struct config *cfg, const char *protocol_in,
 	const char *port_in, int extra_in, const char *protocol_out,
 	const char *port_out, int extra_out, int base, int range)
 {
@@ -63,21 +66,21 @@ static int config_directive(struct config *c, const char *protocol_in,
 	struct ccreader *reader;
 	struct ccwriter *writer;
 
-	log_println(c->log, "config: %s %s %d -> %s %s %d %d %d", protocol_in,
+	log_println(cfg->log, "config: %s %s %d -> %s %s %d %d %d", protocol_in,
 		port_in, extra_in, protocol_out, port_out, extra_out,
 		base, range);
-	chn_in = config_get_channel(c, port_in, extra_in);
+	chn_in = config_get_channel(cfg, port_in, extra_in);
 	if(chn_in == NULL)
 		goto fail;
 	if(chn_in->reader == NULL) {
-		reader = ccreader_create(chn_in->name, protocol_in, c->log);
+		reader = ccreader_create(chn_in->name, protocol_in, cfg->log);
 		chn_in->reader = reader;
-		reader->packet.counter = c->counter;
+		reader->packet.counter = cfg->counter;
 	} else {
 		/* FIXME: check for redefined protocol */
 		reader = chn_in->reader;
 	}
-	chn_out = config_get_channel(c, port_out, extra_out);
+	chn_out = config_get_channel(cfg, port_out, extra_out);
 	if(chn_out == NULL)
 		goto fail;
 	writer = ccwriter_create(chn_out, protocol_out, base, range);
@@ -89,15 +92,15 @@ fail:
 	return -1;
 }
 
-static void config_skip_comments(struct config *c) {
+static void config_skip_comments(struct config *cfg) {
 	int i;
 	for(i = 0; i < LINE_LENGTH; i++) {
-		if(c->line[i] == '#')
-			c->line[i] = '\0';
+		if(cfg->line[i] == '#')
+			cfg->line[i] = '\0';
 	}
 }
 
-static int config_scan_directive(struct config *c) {
+static int config_scan_directive(struct config *cfg) {
 	int i;
 	char protocol_in[16], protocol_out[16];
 	char port_in[16], port_out[16];
@@ -106,38 +109,38 @@ static int config_scan_directive(struct config *c) {
 	int base = 0;
 	int range = 0;
 
-	i = sscanf(c->line, "%15s %15s %d %15s %15s %d %d %d", protocol_in,
+	i = sscanf(cfg->line, "%15s %15s %d %15s %15s %d %d %d", protocol_in,
 		port_in, &extra_in, protocol_out, port_out, &extra_out, &base,
 		&range);
 	if(i == 5)
 		extra_out = extra_in;
 	if(i > 4)
-		return config_directive(c, protocol_in, port_in, extra_in,
+		return config_directive(cfg, protocol_in, port_in, extra_in,
 			protocol_out, port_out, extra_out, base, range);
 	else if(i <= 0)
 		return 0;
 	else {
-		log_println(c->log, "Invalid directive: %s", c->line);
+		log_println(cfg->log, "Invalid directive: %s", cfg->line);
 		return -1;
 	}
 }
 
-int config_read(struct config *c) {
-	FILE *f = fopen(c->filename, "r");
+int config_read(struct config *cfg) {
+	FILE *f = fopen(cfg->filename, "r");
 	if(f == NULL)
 		return -1;
 
-	while(fgets(c->line, LINE_LENGTH, f) == c->line) {
-		config_skip_comments(c);
-		if(config_scan_directive(c))
+	while(fgets(cfg->line, LINE_LENGTH, f) == cfg->line) {
+		config_skip_comments(cfg);
+		if(config_scan_directive(cfg))
 			goto fail;
 	}
-	if(c->n_channels == 0) {
-		log_println(c->log, "Error reading configuration file: %s",
-			c->filename);
+	if(cfg->n_channels == 0) {
+		log_println(cfg->log, "Error reading configuration file: %s",
+			cfg->filename);
 	}
 	fclose(f);
-	return c->n_channels;
+	return cfg->n_channels;
 fail:
 	fclose(f);
 	return -1;
