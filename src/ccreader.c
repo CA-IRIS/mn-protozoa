f38bef4bc634b3b94ec509994767c2018518674f
Douglas Lau (doug.lau@dot.state.mn.us) <Douglas Lau (doug.lau@dot.state.mn.us)>
2007-01-03 15:13:43 -0600

    Added comments to ccreader module.

--- src/ccreader.c
+++ src/ccreader.c
@@ -4,6 +4,12 @@
 #include "pelco_d.h"
 #include "vicon.h"
 
+/*
+ * ccreader_set_protocol	Set protocol of the camera control reader.
+ *
+ * protocol: protocol name
+ * return: 0 on success; -1 if protocol not found
+ */
 static int ccreader_set_protocol(struct ccreader *rdr, const char *protocol) {
 	if(strcasecmp(protocol, "manchester") == 0)
 		rdr->do_read = manchester_do_read;
@@ -18,6 +24,13 @@ static int ccreader_set_protocol(struct ccreader *rdr, const char *protocol) {
 	return 0;
 }
 
+/*
+ * ccreader_init	Initialize a new camera control reader.
+ *
+ * log: message logger
+ * protocol: protocol name
+ * return: pointer to struct ccreader on success; NULL on failure
+ */
 static struct ccreader *ccreader_init(struct ccreader *rdr, struct log *log,
 	const char *protocol)
 {
@@ -30,6 +43,13 @@ static struct ccreader *ccreader_init(struct ccreader *rdr, struct log *log,
 		return rdr;
 }
 
+/*
+ * ccreader_new		Construct a new camera control reader.
+ *
+ * log: message logger
+ * protocol: protocol name
+ * return: pointer to struct ccreader on success; NULL on failure
+ */
 struct ccreader *ccreader_new(const char *name, struct log *log,
 	const char *protocol)
 {
@@ -45,11 +65,21 @@ fail:
 	return NULL;
 }
 
+/*
+ * ccreader_add_writer		Add a writer to the camera control reader.
+ *
+ * wtr: camera control writer to link with the reader
+ */
 void ccreader_add_writer(struct ccreader *rdr, struct ccwriter *wtr) {
 	wtr->next = rdr->writer;
 	rdr->writer = wtr;
 }
 
+/*
+ * ccreader_do_writers		Write a packet to all linked writers.
+ *
+ * return: number of writers that wrote the packet
+ */
 static inline unsigned int ccreader_do_writers(struct ccreader *rdr) {
 	unsigned int res = 0;
 	struct ccpacket *pkt = &rdr->packet;
@@ -66,6 +96,11 @@ static inline unsigned int ccreader_do_writers(struct ccreader *rdr) {
 	return res;
 }
 
+/*
+ * ccreader_process_packet	Process a packet from the camera control reader.
+ *
+ * return: number of writers that wrote the packet
+ */
 unsigned int ccreader_process_packet(struct ccreader *rdr) {
 	struct ccpacket *pkt = &rdr->packet;
 	unsigned int res = 0;
