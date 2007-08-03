acc38a044d62a0bff310c4f092642fba9ee290b6
Douglas Lau (doug.lau@dot.state.mn.us) <Douglas Lau (doug.lau@dot.state.mn.us)>
2007-08-03 14:01:45 -0500

    Added GPL license header to python files.

--- proto/bit_array.py
+++ proto/bit_array.py
@@ -1,4 +1,16 @@
-
+# protozoa -- CCTV transcoder / mixer for PTZ
+# Copyright (C) 2006-2007  Minnesota Department of Transportation
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
 class BitArray(object):
 
 	def __init__(self, n_bits):
