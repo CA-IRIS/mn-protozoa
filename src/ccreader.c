d6182e9a54f76fbadf967a716a2e5c28ee905857
Douglas Lau (doug.lau@dot.state.mn.us) <Douglas Lau (doug.lau@dot.state.mn.us)>
2008-11-17 10:10:24 -0600

    Added a FIXME.

--- src/ccreader.c
+++ src/ccreader.c
@@ -39,7 +39,7 @@ static int ccreader_set_protocol(struct ccreader *rdr, const char *protocol) {
 	} else if(strcasecmp(protocol, "manchester") == 0) {
 		rdr->do_read = manchester_do_read;
 		ccreader_set_timeout(rdr, MANCHESTER_TIMEOUT);
-// Hack for IRIS output
+/* FIXME: Hack for IRIS output */
 ccreader_set_timeout(rdr, 400);
 	} else if(strcasecmp(protocol, "pelco_d") == 0) {
 		rdr->do_read = pelco_d_do_read;
