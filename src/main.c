d073f4d6b88976691eda2ee9cdac7bcd951a25a9
Douglas Lau (doug.lau@state.mn.us) <Douglas Lau (doug.lau@state.mn.us)>
2010-03-08 09:42:38 -0600

    Added --dryrun arg to validate /etc/protozoa.conf

--- src/main.c
+++ src/main.c
@@ -1,6 +1,6 @@
 /*
  * protozoa -- CCTV transcoder / mixer for PTZ
- * Copyright (C) 2006-2009  Minnesota Department of Transportation
+ * Copyright (C) 2006-2010  Minnesota Department of Transportation
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -19,58 +19,31 @@
 #include "config.h"
 #include "poller.h"
 
-#define VERSION "0.36"
-#define BANNER "protozoa: v" VERSION "  Copyright (C) 2006-2009  Mn/DOT"
+#define VERSION "0.37"
+#define BANNER "protozoa: v" VERSION "  Copyright (C) 2006-2010  Mn/DOT"
 
 static const char *CONF_FILE = "/etc/protozoa.conf";
 static const char *LOG_FILE = "/var/log/protozoa";
 
-struct poller *create_poller(struct log *log) {
-	int n_channels;
-	struct config cfg;
-	struct poller *poll;
-	struct packet_counter *cnt;
-
-	poll = malloc(sizeof(struct poller));
-	if(poll == NULL)
-		goto fail;
-	if(log->stats)
-		cnt = packet_counter_new(log);
-	else
-		cnt = NULL;
-	if(config_init(&cfg, log, cnt) == NULL)
-		goto fail;
-	n_channels = config_read(&cfg, CONF_FILE);
-	if(n_channels <= 0) {
-		log_println(log, "Check configuration file: %s", CONF_FILE);
-		goto fail_0;
-	}
-	if(poller_init(poll, n_channels, config_cede_channels(&cfg),
-		cfg.defer) == NULL)
-	{
-		goto fail_0;
-	}
-	config_destroy(&cfg);
-	return poll;
-fail_0:
-	config_destroy(&cfg);
-fail:
-	free(poll);
-	return NULL;
-}
-
 int main(int argc, char* argv[]) {
 	int i;
-	struct poller *poll;
+	int rc;
+	struct poller poll;
+	struct packet_counter *counter;
 	struct log log;
+	struct config cfg;
 	bool daemonize = false;
+	bool dryrun = false;
 
+	rc = 0;
 	log_init(&log);
 	for(i = 0; i < argc; i++) {
 		if(strcmp(argv[i], "--daemonize") == 0)
 			daemonize = true;
 		if(strcmp(argv[i], "--debug") == 0)
 			log.debug = true;
+		if(strcmp(argv[i], "--dryrun") == 0)
+			dryrun = true;
 		if(strcmp(argv[i], "--packet") == 0)
 			log.packet = true;
 		if(strcmp(argv[i], "--stats") == 0)
@@ -79,21 +52,45 @@ int main(int argc, char* argv[]) {
 	if(daemonize) {
 		if(log_open_file(&log, LOG_FILE) == NULL) {
 			log_println(&log, "Cannot open: %s", LOG_FILE);
-			goto fail;
+			rc = (errno ? errno : -1);
+			goto out;
 		}
 	}
 	log_println(&log, BANNER);
-	poll = create_poller(&log);
-	if(poll == NULL)
-		goto fail;
-	if(daemonize) {
-		if(daemon(0, 0) < 0)
-			goto fail;
+	if(log.stats)
+		counter = packet_counter_new(&log);
+	else
+		counter = NULL;
+	if(config_init(&cfg, &log, counter) == NULL) {
+		rc = (errno ? errno : -1);
+		goto out;
 	}
-	poller_loop(poll);
-fail:
-	if(errno)
-		log_println(&log, "Error: %s", strerror(errno));
+	if(config_read(&cfg, CONF_FILE) <= 0) {
+		log_println(&log, "Check configuration file: %s", CONF_FILE);
+		rc = (errno ? errno : -1);
+		goto out_0;
+	}
+	if(dryrun)
+		goto out_0;
+	if(poller_init(&poll, cfg.n_channels, config_cede_channels(&cfg),
+		cfg.defer) == NULL)
+	{
+		rc = (errno ? errno : -1);
+		goto out_0;
+	}
+	if(daemonize) {
+		if(daemon(0, 0) < 0) {
+			rc = (errno ? errno : -1);
+			goto out_0;
+		}
+	}
+	rc = poller_loop(&poll);
+out_0:
+	config_destroy(&cfg);
+	free(counter);
+out:
+	if(rc > 0)
+		log_println(&log, "Error: %s", strerror(rc));
 	log_destroy(&log);
-	return errno;
+	return rc;
 }
